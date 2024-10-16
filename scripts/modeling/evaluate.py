9d96998e2d6e2abe436d37e16560a26d5e6dd22a
Revo Tesha <revogatustesha@gmail.com>
2024-10-16 16:28:30 -0400

    remove if __name__ clause in evaluate.py

--- scripts/modeling/evaluate.py
+++ scripts/modeling/evaluate.py
@@ -41,61 +41,61 @@ model.load_state_dict(torch.load(f"models/{MODEL_NAME}"))
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 model = model.to(device)
 
-if __name__ == "__main__":
-    EOS = 30523
-    trans_samples = []
-    rouge = Rouge(variants=["L", 3, 2], multiref="best")
+EOS = 30523
 
-    valid_data = Dataset.from_dict(read_corpus("data/valid_data.jsonl"))
+trans_samples = []
+rouge = Rouge(variants=["L", 3, 2], multiref="best")
 
-    for src_batch, trg_batch in tqdm(
-        zip(src_valid_loader, trg_valid_loader), total=len(src_valid_loader)
-    ):
-        src_x = src_batch["input_ids"].T.to(device)
-        src_key_padding_mask = (src_batch["attention_mask"] == 0).to(device)
-        trg_x = trg_batch["input_ids"][:, 0].unsqueeze(0).to(device)
+valid_data = Dataset.from_dict(read_corpus("data/valid_data.jsonl"))
 
-        model.eval()
-        output, src_x = model(
+for src_batch, trg_batch in tqdm(
+    zip(src_valid_loader, trg_valid_loader), total=len(src_valid_loader)
+):
+    src_x = src_batch["input_ids"].T.to(device)
+    src_key_padding_mask = (src_batch["attention_mask"] == 0).to(device)
+    trg_x = trg_batch["input_ids"][:, 0].unsqueeze(0).to(device)
+
+    model.eval()
+    output, src_x = model(
+        src_x,
+        trg_x,
+        src_key_padding_mask,
+        trg_key_padding_mask=None,
+        return_encoder_output=True,
+    )
+    output_ids = output.argmax(-1)
+
+    src_seq_length = src_batch["attention_mask"].shape[1]
+    for idx in range(src_seq_length):
+        trg_x = torch.cat((trg_x, output_ids[idx, :].unsqueeze(0)))
+
+        output = model(
             src_x,
             trg_x,
             src_key_padding_mask,
             trg_key_padding_mask=None,
-            return_encoder_output=True,
+            decoder_only=True,
         )
         output_ids = output.argmax(-1)
 
-        src_seq_length = src_batch["attention_mask"].shape[1]
-        for idx in range(src_seq_length):
-            trg_x = torch.cat((trg_x, output_ids[idx, :].unsqueeze(0)))
+        if output_ids.T.squeeze(0)[-1].item() == EOS or idx == src_seq_length - 1:
+            candidate = (trg_x.T.squeeze(0)[1:]).tolist()
+            break
 
-            output = model(
-                src_x,
-                trg_x,
-                src_key_padding_mask,
-                trg_key_padding_mask=None,
-                decoder_only=True,
-            )
-            output_ids = output.argmax(-1)
+    reference = trg_batch["input_ids"][:, 1:].squeeze()
+    padding_index = torch.where(reference == EOS)[0].item()
+    reference = (reference[:padding_index]).tolist()
 
-            if output_ids.T.squeeze(0)[-1].item() == EOS or idx == src_seq_length - 1:
-                candidate = (trg_x.T.squeeze(0)[1:]).tolist()
-                break
+    reference = [[str(r) for r in reference]]
+    candidate = [str(id) for id in candidate]
 
-        reference = trg_batch["input_ids"][:, 1:].squeeze()
-        padding_index = torch.where(reference == EOS)[0].item()
-        reference = (reference[:padding_index]).tolist()
+    rouge.update(([candidate], [reference]))
 
-        reference = [[str(r) for r in reference]]
-        candidate = [str(id) for id in candidate]
+    number = random.randint(0, 64)
+    if number == 4:
+        trans_samples.append((candidate, reference))
 
-        rouge.update(([candidate], [reference]))
-
-        number = random.randint(0, 64)
-        if number == 4:
-            trans_samples.append((candidate, reference))
-
-    results = rouge.compute()
-    with open(f"results/results_{MODEL_NAME.strip('.pth')}.txt", "w") as f:
-        for metric, score in results.items():
-            f.write(f"{metric}: {score}\n")
+results = rouge.compute()
+with open(f"results/results_{MODEL_NAME.strip('.pth')}.txt", "w") as f:
+    for metric, score in results.items():
+        f.write(f"{metric}: {score}\n")
